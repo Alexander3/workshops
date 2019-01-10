7279caa02b8323185d280acda30bbcc7178dd343
Aleksander <kawalaaleksander@gmail.com>
2019-01-10 16:57:40 +0100

    adding dynamic data to display

--- django/firstapp/views.py
+++ django/firstapp/views.py
@@ -1,5 +1,11 @@
+from django.utils.timezone import now
 from django.views.generic import TemplateView
 
 
 class AboutView(TemplateView):
     template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'time': now().isoformat()
+        }
