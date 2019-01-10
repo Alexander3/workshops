cdd36a9311bca3c793de263a25e347d23421ab75
Aleksander <kawalaaleksander@gmail.com>
2019-01-10 16:52:43 +0100

    Adding view

--- django/hello_django/urls.py
+++ django/hello_django/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from firstapp.views import AboutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('about/', AboutView.as_view()),
 ]
